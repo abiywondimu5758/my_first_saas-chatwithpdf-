b291e69543c2987e29546adc8e77b171a9002259
Abiywondimu5758 <abiywondimu1@gmail.com>
2023-11-06 18:44:44 +0300

    database done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { UserButton, auth } from "@clerk/nextjs";
 import { Button } from "../components/ui/button";
 import Link from "next/link";
 import {  LogIn } from "lucide-react";
+import FileUpload from "@/components/FileUpload";
 export default async function Home() {
   const {userId} = await auth();
   const isAuth = !!userId
@@ -18,7 +19,7 @@ export default async function Home() {
           </div>
           <p className="max-w-xl mt-2 text-lg text-slate-600">Join millions of students, researchers and professionals and answer questions and understand research with AI</p>
           <div className="w-full mt-4">
-            {isAuth ? (<h1>fileupload</h1>):(<Link href='/sign-in'>
+            {isAuth ? (<FileUpload/>):(<Link href='/sign-in'>
             <Button>Login to get started
               <LogIn className='w-4 h-4 ml-2'/></Button></Link>)}
           </div>
